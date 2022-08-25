1818b5ca0249f01b3878191a6d601dc869598cee
mowangjuanzi <baoguoxiao0538@hotmail.com>
2022-08-26 07:59:44 +0800

    update

--- framework/Application.php
+++ framework/Application.php
@@ -2,16 +2,28 @@
 
 namespace Melon;
 
-use JetBrains\PhpStorm\NoReturn;
 use Symfony\Component\Yaml\Yaml;
 
 class Application
 {
-    protected readonly array $config;
+    /**
+     * @var array
+     */
+    protected array $workers = [];
 
-    public function __construct(protected string $app)
+    /**
+     * @var array
+     */
+    public array $config = [];
+
+    /**
+     * 构造函数
+     *
+     * @param string $app
+     */
+    public function __construct(protected readonly string $app)
     {
-        $this->config = Yaml::parseFile($this->app . "/config/melon.yaml");
+        $this->reloadConfig();
     }
 
     /*
@@ -21,22 +33,61 @@ class Application
     {
         $daemon = $this->config['daemon'] ?? 'on';
 
-        if ($daemon == 'on') {
-            $pid = pcntl_fork();
-
-            if ($pid == -1) {
-                dd("fork worker process failed");
-            } elseif ($pid) {
-                pcntl_wait($status);
-            } else {
-                $worker = new Worker();
-            }
-        }
+//        if ($daemon == 'on') {
+//            $pid = pcntl_fork();
+//
+//            if ($pid == -1) {
+//                dd("fork worker process failed");
+//            } elseif ($pid) {
+//                $this->workers[] = $pid;
+//            } else {
+        $worker = new Worker($this);
+        $worker->run();
+//            }
+//        }
     }
 
-    #[NoReturn]
+    /**
+     * 运行
+     *
+     * @return void
+     */
     public function run(): void
     {
         $this->fork();
+    }
+
+    /**
+     * 获取配置
+     *
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * 检查配置 重载配置
+     *
+     * @return void
+     */
+    public function reloadConfig(): void
+    {
+        $config = Yaml::parseFile($this->app . "/config/melon.yaml");
+
+        $status = $this->configCheck($config);
+
+        if ($status) {
+            $this->config = $config;
+        }
+    }
+
+    /**
+     * 配置校验
+     */
+    protected function configCheck(array $config): bool
+    {
+        return true;
     }
 }
--- framework/Worker.php
+++ framework/Worker.php
@@ -4,8 +4,47 @@ namespace Melon;
 
 class Worker
 {
-    public function __construct()
-    {
+    protected array $ports;
 
+    protected array $sockets;
+
+    public function __construct(protected readonly Application $application)
+    {
+        // 获取配置
+        $config = $this->application->getConfig();
+
+        // 获取需要实例化的端口
+        $this->getPorts($config);
+
+        $eventConfig = new \EventConfig();
+
+        $eventBase = new \EventBase($eventConfig);
+
+        $event = new \Event($eventBase, array_shift($this->sockets), \Event::SIGNAL | \Event::READ);
+    }
+
+    /**
+     * 格式化配置，通过端口来映射配置
+     */
+    protected function getPorts(array $config): void
+    {
+        $this->ports = [];
+        foreach ($config['http']['server'] as $http) {
+            $this->ports[$http['listen']][] = $http;
+        }
+    }
+
+    protected function run()
+    {
+        // TODO 挖个坑，这里要支持一个 Worker 绑定多个 socket
+        foreach ($this->ports as $port => $tmp) {
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            socket_bind($socket, '0.0.0.0', $port);
+//            socket_set_option($socket, SOL_SOCKET, SO_REUSEPORT, 1); // reuseport
+
+            $this->sockets[$port] = $socket;
+
+
+        }
     }
 }
